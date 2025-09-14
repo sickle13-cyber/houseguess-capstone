6dcabeb1822447a7a92b466bbf4d23ee1ac4fb50
Preeth Vijay <preethvijay0@gmail.com>
2025-09-14 09:26:55 -0400

    create util.py to store helpers

---
+++ houseguess/util.py
@@ -0,0 +1,47 @@
+from math import radians, sin, cos, asin, sqrt
+from datetime import datetime
+import requests
+import shutil
+import os
+
+def haversine_km(a_lat: float, a_lon: float, b_lat: float, b_lon: float) -> float:
+    """Great-circle distance in kilometers."""
+    R = 6371.0088
+    dlat = radians(b_lat - a_lat)
+    dlon = radians(b_lon - a_lon)
+    la1, la2 = radians(a_lat), radians(b_lat)
+    h = sin(dlat / 2) ** 2 + cos(la1) * cos(la2) * sin(dlon / 2) ** 2
+    return 2 * R * asin(sqrt(h))
+
+def download_img(url: str) -> str:
+    """ Returns filename for an image after downloading it, if successful."""
+
+    prefix = "assets/images"
+    if not os.path.isdir(prefix):
+        os.mkir(prefix)
+
+    try:
+        response = requests.get(url, stream=True)
+        
+        # Verifies status == 200.
+        response.raise_for_status() 
+
+        # Generate date based filename for image.
+        now = datetime.now()
+        formatted_string = now.strftime("%Y_%m_%d-%H_%M_%S")
+        count = os.listdir(prefix)
+        file_name = f"{formatted_string}_{count + 1}.png"
+
+        # Save image.
+        save_path = f"assets/images/{file_name}"
+        with open(save_path, 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+
+        print(f"[DEBUG] Image successfully downloaded to: {save_path}")
+        return save_path
+    except requests.exceptions.RequestException as e:
+        print(f"[DEBUG] Error downloading image: {e}")
+    except IOError as e:
+        print(f"[DEBUG] Error saving image to file: {e}")
+
+    return ""

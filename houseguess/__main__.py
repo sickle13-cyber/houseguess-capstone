efb29d9f0aaa9021998e2b36939d36c1f64911a9
sickle13-cyber <collin.poag@yahoo.com>
2025-09-21 19:18:57 +0000

    Update __main__.py
    Added comments. May need to remove comment over if check.

--- houseguess/__main__.py
+++ houseguess/__main__.py
@@ -1,4 +1,13 @@
+"""
+Project: HouseGuess
+Authors: Preeth Vijay, Haytham Moussa, Connor Pollack, Victor Ortiz Nazario, Sam Appiah, Collin Poag
+Date: 9/21/2025
+Description: This file serves as the entrypoint for HouseGuess
+"""
+
+"""Libraries"""
 from .app import HouseGuessMain
 
+"""Start HouseGuess"""
 if __name__ == "__main__":
     HouseGuessMain()
